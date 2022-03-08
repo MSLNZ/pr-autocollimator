7f363020df7d6b6ec2c87c9f712a209c942cfb1b
jborbely <joe.borbely@gmail.com>
2022-03-08 13:04:02 +1300

    add kwargs to the crosshair() function

--- autocollimator/__init__.py
+++ autocollimator/__init__.py
@@ -17,11 +17,53 @@ version_info = namedtuple('version_info', 'major minor micro releaselevel')(int(
 """:obj:`~collections.namedtuple`: Contains the version information as a (major, minor, micro, releaselevel) tuple."""
 
 
-def crosshair(host='pr-autocollimator'):
-    """Locate the crosshair."""
-    reply = requests.get(f'http://{host}/crosshair')
-    json = reply.json()
+def crosshair(*, host='pr-autocollimator', debug=False, show=False,
+              origin=None, threshold=None, pixels_per_arcmin=None):
+    """Fetch information about the current location of the crosshair.
 
+    Parameters
+    ----------
+    host : :class:`str`, optional
+        The hostname or IP address of the Raspberry Pi.
+    debug : :class:`bool`, optional
+        Whether to return a binary image of the localized crosshair and the
+        projections along the x and y axes as an <img> html tag.
+    show : :class:`bool`, optional
+        Whether to return an image of the localized crosshair as an
+        <img> html tag.
+    origin : :class:`list`, optional
+        The [x, y] location of the origin in pixel units.
+    threshold : :class:`int`, optional
+        A value between [0, 255] to filter the crosshair from the image.
+    pixels_per_arcmin : :class:`float`, optional
+        The conversion factor to convert pixel units to arcmin units.
+
+    Returns
+    -------
+    :class:`dict` or :class:`str`
+        If `debug` or `show` is enabled then the <img> html tag as
+        a string. Otherwise, a dictionary containing the location of the
+        crosshair and the image is returned.
+    """
+    params = {}
+    if debug:
+        params['debug'] = 1
+    if show:
+        params['show'] = 1
+    if origin:
+        params['origin'] = f'{origin[0]},{origin[1]}'
+    if threshold:
+        params['threshold'] = str(threshold)
+    if pixels_per_arcmin:
+        params['pixels_per_arcmin'] = str(pixels_per_arcmin)
+
+    reply = requests.get(f'http://{host}/crosshair', params=params)
+    reply.raise_for_status()
+
+    if debug or show:
+        return reply.content
+
+    json = reply.json()
     buffer = BytesIO(b64decode(json['image']))
     arr = np.frombuffer(buffer.getvalue(), dtype=np.uint8)
     image = cv.imdecode(arr, flags=cv.IMREAD_UNCHANGED)
